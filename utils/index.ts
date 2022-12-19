8d45451f54d1716217235bae4e24ef08d073d7f6
yuzhaojun <3226033807@qq.com>
2022-12-19 23:25:10 +0800

    feat: utils

--- utils/index.ts
+++ utils/index.ts
@@ -3,3 +3,6 @@ export type Equal<T, U> = (<T>() => T extends U ? true : false) extends (<U>() =
 export type Expect<T> = T extends true ? true : false
 
 
+export type MergeInsertions<T> = T extends object ? { [K in keyof T]: MergeInsertions<T[K]> } : T
+
+export type Alike<X, Y> = Equal<MergeInsertions<X>, MergeInsertions<Y>>
